f88174ad856bf3ef13ad5edf327f325bac13dbf0
Neal Hardesty <nealhardesty@yahoo.com>
2016-08-02 17:15:52 -0700

    Switched kafka over to Confluent platform for ease

--- kafka/lib/start_kafka.sh
+++ kafka/lib/start_kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /opt/kafka
-bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-bin/kafka-server-start.sh config/server.properties
+/usr/bin/zookeeper-server-start -daemon /etc/kafka/zookeeper.properties
+(sleep 10 && /usr/bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties) &
+(sleep 10 && /usr/bin/schema-registry-start /etc/schema-registry/schema-registry.properties) &
+exec /usr/bin/kafka-server-start /etc/kafka/server.properties
