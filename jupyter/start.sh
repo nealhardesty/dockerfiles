fe1345b2a9930d7cb42c88c8b95e6702bff81e6e
Neal Hardesty <neal@crunchbase.com>
2019-04-12 15:48:52 -0700

    Now with ssl in jupyter

--- jupyter/start.sh
+++ jupyter/start.sh
@@ -1,9 +1,18 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
+
+set -ex
+
+[[ -z "$BUILD" ]] && echo set BUILD to rebuild
+[[ -z "$PASSWORD" ]] && echo set PASSWORD on build to override default
 
 if [[ ! -z "$BUILD" ]]; then
-  docker build -t nealhardesty/jupyter $(dirname $0)
+  docker build --build-arg PASSWORD=${PASSWORD} -t nealhardesty/jupyter $(dirname $0)
 fi
+
 mkdir -p ~/notebooks
-chmod 1777 ~/notebooks
-docker rm jupyter
-docker run -d -ti --init --rm --name jupyter -v ~/notebooks:/var/notebooks -p 8888:8888 nealhardesty/jupyter
+
+docker stop jupyter || echo was not running
+docker rm jupyter || echo did not exist
+
+docker run -d -ti --init --rm --name jupyter -v ~/notebooks:/home/jupyter/notebooks -p 8888:8888 nealhardesty/jupyter
+#docker run -ti --init --rm --name jupyter -v ~/:/home/jupyter -p 8888:8888 nealhardesty/jupyter
